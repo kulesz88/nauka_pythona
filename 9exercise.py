ecc56dbb5773312e4d5c4d4abad4965c5ae26a00
kulesz88 <kulesz88@users.noreply.github.com>
2018-10-13 12:24:51 +0200

    First upload on Saturday

---
+++ 9exercise.py
@@ -0,0 +1,10 @@
+rowery = ['unibike', 'romet']
+
+print(rowery[0])
+print(rowery[1])
+
+for s in rowery:
+    print(s)
+
+for idx in range( len(rowery) ) :
+    print("idx: " + str(idx) + " : " + rowery[idx])
